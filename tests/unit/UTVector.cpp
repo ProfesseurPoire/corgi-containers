59cb237f9a4182f8cceeef1d3cea352a2bd641e5
ProfesseurPoire <guyot.ced@gmail.com>
2023-02-28 14:59:01 +0100

    add warnings

--- tests/unit/UTVector.cpp
+++ tests/unit/UTVector.cpp
@@ -11,75 +11,76 @@ using namespace corgi::test;
 class Dummy
 {
 public:
-
-	Dummy(int a, float b):a(a),b(b){}
-	int a;
-	float b;
+    Dummy(int a, float b)
+        : a(a)
+        , b(b)
+    {
+    }
+    int   a;
+    float b;
 };
-
 
 class Foo
 {
 public:
-
-	Foo(float f, int v):f(f),v(v){}
-	float f;
-	int v;
+    Foo(float f, int v)
+        : f(f)
+        , v(v)
+    {
+    }
+    float f;
+    int   v;
 };
 
 class DummyVector
 {
 public:
+    DummyVector(float f, int v)
+    {
+        std::cout << "Constructor" << std::endl;
+        datas.push_back(Foo(f, v));
+    }
 
-	DummyVector(float f, int v)
-	{
-		std::cout<<"Constructor"<< std::endl;
-		datas.push_back(Foo(f, v));
-	}
-
-	DummyVector(const DummyVector& v)
-	{
-		datas = v.datas;
+    DummyVector(const DummyVector& v)
+    {
+        datas = v.datas;
         //std::cout<<"Copy Constructor"<< std::endl;
-	}
+    }
 
-	DummyVector(DummyVector&& other)
-	{
-		datas = std::move(other.datas);
-		//std::cout<< " Move Constructor"<< std::endl;
-	}
+    DummyVector(DummyVector&& other)
+    {
+        datas = std::move(other.datas);
+        //std::cout<< " Move Constructor"<< std::endl;
+    }
 
-
-	DummyVector& operator=(DummyVector&& other)
-	{
-		datas = std::move(other.datas);
+    DummyVector& operator=(DummyVector&& other)
+    {
+        datas = std::move(other.datas);
         //std::cout<< " Move Operator"<< std::endl;
-		return *this;
-	}
+        return *this;
+    }
 
-	DummyVector& operator=(const DummyVector& other)
-	{
-		datas = other.datas;
+    DummyVector& operator=(const DummyVector& other)
+    {
+        datas = other.datas;
         //std::cout<< " Copy Operator"<< std::endl;
-		return *this;
-	}
-	
-	~DummyVector()
-	{
+        return *this;
+    }
+
+    ~DummyVector()
+    {
         //std::cout << "destructor" << std::endl;
-	}
+    }
 
-	std::vector<Foo> datas;
+    std::vector<Foo> datas;
 };
-
 
 class VectorFixture : public Test
 {
 public:
-
-	Vector<Dummy> v;
-	Vector<Dummy> v2;
-	Vector<DummyVector> v3;
+    Vector<Dummy>       v;
+    Vector<Dummy>       v2;
+    Vector<DummyVector> v3;
 };
 
 //
@@ -145,7 +146,6 @@ public:
 //	assert_that(v.size(), equals(0));
 //}
 
-
 //TEST_F(VectorFixture, test_corruption)
 //{
 //	Vector<int> v;
@@ -158,124 +158,123 @@ public:
 
 TEST_F(VectorFixture, test_stl2)
 {
-	std::vector<DummyVector> a;
+    std::vector<DummyVector> a;
 
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,1);
-	
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,2);
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0F, 1);
 
-	std::vector<DummyVector> b;
-	
-	std::cout<<"Moving a into b"<<std::endl;
-	b=std::move(a);
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0F, 2);
+
+    std::vector<DummyVector> b;
+
+    std::cout << "Moving a into b" << std::endl;
+    b = std::move(a);
 }
 
 TEST_F(VectorFixture, test_v2)
 {
-	Vector<DummyVector> a;
+    Vector<DummyVector> a;
 
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,1);
-	
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,2);
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0f, 1);
 
-	Vector<DummyVector> b;
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0f, 2);
 
-	b.emplace_back(0.0f, 3);
-	
-	std::cout<<"Moving a into b"<<std::endl;
-	b = std::move(a);
+    Vector<DummyVector> b;
+
+    b.emplace_back(0.0f, 3);
+
+    std::cout << "Moving a into b" << std::endl;
+    b = std::move(a);
 }
 
 TEST_F(VectorFixture, test_stl)
 {
-	std::vector<DummyVector> a;
+    std::vector<DummyVector> a;
 
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,1);
-	
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,2);
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0f, 1);
 
-	std::cout<<"Erase "<<std::endl;
-	a.erase(a.begin());
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0f, 2);
 
-	std::vector<DummyVector> b;
-	
-	b.emplace_back(0.0f, 3);
-	std::cout<<"Copying a into b"<<std::endl;
-	b=a;
+    std::cout << "Erase " << std::endl;
+    a.erase(a.begin());
+
+    std::vector<DummyVector> b;
+
+    b.emplace_back(0.0f, 3);
+    std::cout << "Copying a into b" << std::endl;
+    b = a;
 }
 
 TEST_F(VectorFixture, testnotstl)
 {
-	Vector<DummyVector> a;
+    Vector<DummyVector> a;
 
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,1);
-	
-	std::cout<<"Emplace back"<<std::endl;
-	a.emplace_back(0.0f,2);
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0f, 1);
 
-	
-	std::cout<<"Erase "<<std::endl;
-	a.remove_at(0);
-	
-	Vector<DummyVector> b;
-	
-	std::cout<<"Copying a into b"<<std::endl;
-	b=a;
+    std::cout << "Emplace back" << std::endl;
+    a.emplace_back(0.0f, 2);
+
+    std::cout << "Erase " << std::endl;
+    a.remove_at(0);
+
+    Vector<DummyVector> b;
+
+    std::cout << "Copying a into b" << std::endl;
+    b = a;
 }
 
 TEST_F(VectorFixture, testDummyVector)
 {
-	std::vector<int> a{1};
-	a.erase(a.begin(), a.end());
-	a.erase(a.begin(), a.end());
+    std::vector<int> a {1};
+    a.erase(a.begin(), a.end());
+    a.erase(a.begin(), a.end());
 
-	v3.emplace_back(0.0f,1);
+    v3.emplace_back(0.0f, 1);
 
-	v3.at(0).datas.erase(v3.at(0).datas.begin(), v3.at(0).datas.end());
-	v3.at(0).datas.emplace_back(0.0f, 5);
+    v3.at(0).datas.erase(v3.at(0).datas.begin(), v3.at(0).datas.end());
+    v3.at(0).datas.emplace_back(0.0f, 5);
 
-	v3.emplace_back(0.0f, 2);
+    v3.emplace_back(0.0f, 2);
 
-	v3.at(0).datas.erase(v3.at(0).datas.begin(), v3.at(0).datas.end());
+    v3.at(0).datas.erase(v3.at(0).datas.begin(), v3.at(0).datas.end());
 
-	v3.emplace_back(0.0f, 3);
-	v3.emplace_back(2, 2);
+    v3.emplace_back(0.0f, 3);
+    v3.emplace_back(2.0F, 2);
 
-	v3.emplace_back(v3.at(0));
-	v3.back().datas.erase(v3.back().datas.begin(), v3.back().datas.end());
-	v3.emplace_back(1, 1);
+    v3.emplace_back(v3.at(0));
+    v3.back().datas.erase(v3.back().datas.begin(), v3.back().datas.end());
+    v3.emplace_back(1.0F, 1);
 
-	auto cpy = v3;
-	cpy.emplace_back(1, 1);
-	cpy.remove_at(1);
-	cpy.emplace_back(1, 1);
-	cpy.emplace_back(1, 1);
-	cpy.emplace_back(1, 1);
-	cpy.emplace_back(1, 1);
+    auto cpy = v3;
+    cpy.emplace_back(1.0F, 1);
+    cpy.remove_at(1);
+    cpy.emplace_back(1.0F, 1);
+    cpy.emplace_back(1.0F, 1);
+    cpy.emplace_back(1.0F, 1);
+    cpy.emplace_back(1.0F, 1);
 
-	cpy.remove_at(1);
+    cpy.remove_at(1);
 
-	Vector<DummyVector> mv;
-	mv = std::move(cpy);
+    Vector<DummyVector> mv;
+    mv = std::move(cpy);
 
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
 
-	mv.remove_at(2);
+    mv.remove_at(2);
 
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.emplace_back(2, 2);
-	mv.at(0).datas.erase(mv.at(0).datas.begin(), mv.at(0).datas.end());
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.emplace_back(2.0F, 2);
+    mv.at(0).datas.erase(mv.at(0).datas.begin(), mv.at(0).datas.end());
 }
